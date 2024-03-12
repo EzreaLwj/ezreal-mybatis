b8648ade8f4147872ccf80f6defacf170ec28e3e
ezreal <956371950@qq.com>
2024-03-12 13:53:06 +0800

    feat: 通过反射器动态获取属性

--- ezreal-mybatis-07/src/main/java/com/ezreal/mybatis/datasource/pooled/PooledDataSourceFactory.java
+++ ezreal-mybatis-07/src/main/java/com/ezreal/mybatis/datasource/pooled/PooledDataSourceFactory.java
@@ -9,14 +9,7 @@ import javax.sql.DataSource;
  * @Date 2024/3/8
  */
 public class PooledDataSourceFactory extends UnpooledDataSourceFactory {
-
-    @Override
-    public DataSource getDataSource() {
-        PooledDataSource pooledDataSource = new PooledDataSource();
-        pooledDataSource.setDriver(properties.getProperty("driver"));
-        pooledDataSource.setUrl(properties.getProperty("url"));
-        pooledDataSource.setUsername(properties.getProperty("username"));
-        pooledDataSource.setPassword(properties.getProperty("password"));
-        return pooledDataSource;
+    public PooledDataSourceFactory() {
+        this.dataSource = new PooledDataSource();
     }
 }
--- ezreal-mybatis-07/src/main/java/com/ezreal/mybatis/datasource/unpooled/UnpooledDataSourceFactory.java
+++ ezreal-mybatis-07/src/main/java/com/ezreal/mybatis/datasource/unpooled/UnpooledDataSourceFactory.java
@@ -1,6 +1,9 @@
 package com.ezreal.mybatis.datasource.unpooled;
 
 import com.ezreal.mybatis.datasource.DataSourceFactory;
+import com.ezreal.mybatis.datasource.pooled.PooledDataSource;
+import com.ezreal.mybatis.reflection.MetaObject;
+import com.ezreal.mybatis.reflection.SystemMetaObject;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -11,22 +14,44 @@ import java.util.Properties;
  */
 public class UnpooledDataSourceFactory implements DataSourceFactory {
 
-    protected Properties properties;
+    protected DataSource dataSource;
 
+    public UnpooledDataSourceFactory() {
+        this.dataSource = new UnpooledDataSource();
+    }
 
     @Override
-    public void setProperties(Properties properties) {
-        this.properties = properties;
+    public void setProperties(Properties props) {
+        MetaObject metaObject = SystemMetaObject.forObject(dataSource);
+        for (Object key : props.keySet()) {
+            String propertyName = (String) key;
+            if (metaObject.hasSetter(propertyName)) {
+                String value = (String) props.get(propertyName);
+                Object convertValue = convertValue(metaObject, propertyName, value);
+                metaObject.setValue(propertyName, convertValue);
+            }
+        }
+    }
+
+    /**
+     * 根据setter的类型,将配置文件中的值强转成相应的类型
+     */
+    private Object convertValue(MetaObject metaObject, String propertyName, String value) {
+        Object convertedValue = value;
+        Class<?> targetType = metaObject.getSetterType(propertyName);
+        if (targetType == Integer.class || targetType == int.class) {
+            convertedValue = Integer.valueOf(value);
+        } else if (targetType == Long.class || targetType == long.class) {
+            convertedValue = Long.valueOf(value);
+        } else if (targetType == Boolean.class || targetType == boolean.class) {
+            convertedValue = Boolean.valueOf(value);
+        }
+        return convertedValue;
     }
 
     @Override
     public DataSource getDataSource() {
-        UnpooledDataSource unpooledDataSource = new UnpooledDataSource();
-        unpooledDataSource.setUsername(properties.getProperty("username"));
-        unpooledDataSource.setPassword(properties.getProperty("password"));
-        unpooledDataSource.setUrl(properties.getProperty("url"));
-        unpooledDataSource.setDriver(properties.getProperty("driver"));
-        return unpooledDataSource;
+        return dataSource;
     }
 
 }
